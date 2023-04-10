bc58e3cc8175f1638b338c3360191af1b84cc484
Hamza <hamzayusuf26@outlook.com>
2023-04-10 20:43:27 +0100

    changed favicon and doc title

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,17 @@ import "./App.css";
 import AnswerSection from "./components/AnswerSection";
 import CalcButtons from "./components/CalcButtons";
 import { CalcContextProvider, useCalcContext } from "./context/CalcContext";
+import { Helmet } from "react-helmet";
 
 function App() {
   const { checked } = useCalcContext();
   return (
     <div class="app" data-theme={checked}>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Calculator App</title>
+        <link rel="canonical" href="http://mysite.com/example" />
+      </Helmet>
       <div className="calcApp">
         <div className="top-section">
           <AnswerSection />
